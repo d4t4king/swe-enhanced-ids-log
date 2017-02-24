9f39f027daa1a45a1c054f093ca9ac23faa7cab8
dataking <dataking@gmail.com>
2017-02-23 23:11:59 -0800

    WIP; javascript hide added; moved old files to archive

--- usr/lib/smoothwall/langs/en.pl
+++ usr/lib/smoothwall/langs/en.pl
@@ -1,3 +1,8 @@
 %basetr = (
 	%basetr,
+	'summary'		=>	'IDS Event Summary',
+	'num_ruleid'	=>	'Rule IDs by Count:',
+	'num_prios'		=>	'Event Priorities by Count:',
+	'num_class'		=>	'Event Class Types by Count:',
+	'hide summary'	=>	'Hide IDS Event Summary',
 );
