b327018f4ba98541daa2e5ef732de3b5d71ddf2b
dataking <dataking@gmail.com>
2017-02-28 01:33:15 -0800

    updates

--- usr/lib/smoothwall/langs/en.pl
+++ usr/lib/smoothwall/langs/en.pl
@@ -5,4 +5,5 @@
 	'num_prios'		=>	'Event Priorities by Count:',
 	'num_class'		=>	'Event Class Types by Count:',
 	'hide summary'	=>	'Hide IDS Event Summary',
+	'show summary'	=>	'Show IDS Event Summary',
 );
